882aff4e3b445cf4d55f91c5baa622e78014a09c
Asadell <m.satrioasadell@gmail.com>
2024-11-19 13:27:43 +0700

    initial commit

---
+++ src/hospital/HospitalOop.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hospital;
+
+import hospital.gui.HomePage;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class HospitalOop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Asadel Uhuy");
+        new HomePage();
+    }
+    
+}
---
+++ src/hospital/gui/HomePage.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hospital.gui;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class HomePage {
+    private JFrame frame = new JFrame("Home Page");
+    
+}
